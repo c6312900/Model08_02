2e479decce5570c6ba401f8ee972e7156c91d62e
c6312900 <c6312900@gmail.com>
2017-05-25 20:33:44 +0800

    Model08_02

---
+++ Model08_01/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Model08_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HelloWord()
+        {
+            ViewBag.message = "HelloWorld:" + DateTime.Now.ToString();
+            return PartialView(); //cannot us Layout
+        }
+
+        public ActionResult HelloWord2(string username)
+        {
+            ViewBag.message = "HelloWorld:" +username + DateTime.Now.ToString();
+            return PartialView("HelloWord"); //cannot us Layout
+        }
+    }
+}
